9e02ce6fe33777c70dbf6d61b170f693ea6f0b8b
San123desh <suuchang53@gmail.com>
2024-02-28 12:50:55 +0545

    File Handling

--- ConsoleAp/CG_FileIO/FileDirectoryHandling.cs
+++ ConsoleAp/CG_FileIO/FileDirectoryHandling.cs
@@ -1,4 +1,5 @@
 
+using System;
 using System.IO;
 
 class FDHandling
@@ -7,11 +8,33 @@ class FDHandling
     {
         var folderPath = @"C:\6thsem\netcentric\ConsoleAp\CG_FileIO";
         var filePath = $"{folderPath}\\student.txt";
-
-        
-
         File.WriteAllText(filePath,"It is what it is.");
 
+        //to text files format FileA1.txt,FileB2.txt
+        for(var counter = 'A'; counter <= 'J'; counter++)
+        {
+            File.WriteAllText($"{folderPath}\\File{counter}{counter - 64}.txt",DateTime.Now.ToLongDateString());
+        }
+    }
+
+    public void ReadFile()
+    {
+        var filePath = @"C:\6thsem\netcentric\ConsoleAp\CG_FileIO\FileA1.txt";
+        var fileContent = File.ReadAllText(filePath);
+        Console.WriteLine(fileContent);
+
+        //read file and count number of words and sentences
+        var sentences = fileContent.Split(['.','?','!']);
+        Console.WriteLine($"Number of sentences: {sentences.Length}");
+        // foreach(var sentence in sentences){
+        //     Console.WriteLine(sentence);
+        // }
+
+        var words = fileContent.Split([" ","\\n","-",":","\\","."],StringSplitOptions.RemoveEmptyEntries);
+        Console.WriteLine($"Number of words: {words.Length}");
+         foreach(var word in words){
+            Console.WriteLine(word);
+        }
 
     }
 }
