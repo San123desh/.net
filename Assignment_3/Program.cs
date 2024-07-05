409245d24fab5048b36a016e2675501ec6b08afd
San123desh <suuchang53@gmail.com>
2024-07-05 11:19:00 +0545

    updated

--- Assignment_3/Program.cs
+++ Assignment_3/Program.cs
@@ -8,15 +8,38 @@
 //     Console.WriteLine($"{inflationRate.RegionalMember}:{inflationRate.InflationRate}");
 // }
 
-int? highestInflationYear = inflationAnalysis.GetYearWithHighestInflation();
-if (highestInflationYear.HasValue)
-{
-    Console.WriteLine($"\nYear when Nepal had the highest inflation: {highestInflationYear.Value}");
+
+//b
+
+// int? highestInflationYear = inflationAnalysis.GetYearWithHighestInflation();
+// if (highestInflationYear.HasValue)
+// {
+//     Console.WriteLine($"\nYear when Nepal had the highest inflation: {highestInflationYear.Value}");
+// }
+// else
+// {
+//     Console.WriteLine("\nNo data available for Nepal.");
+// }
+
+
+//c
+
+// var top10 = inflationAnalysis.GetTop10RegionsWithHighestInflation();
+// Console.WriteLine("\nTop 10 regions with highest inflation of all time:");
+// foreach (var regions in top10)
+// {
+//     Console.WriteLine($"{regions.RegionalMember}:{regions.InflationRate}");
+// }
+
+
+//d
+
+var top3SACfor2020 = inflationAnalysis.GetTop3SouthAsianCountriesWithLowestInflationForYear(2020);
+Console.WriteLine("\nTop 3 South Asian countries with lowest inflation for 2020:");
+foreach(var country in top3SACfor2020){
+    Console.WriteLine($"{country.RegionalMember}:{country.InflationRate}");
 }
-else
-{
-    Console.WriteLine("\nNo data available for Nepal.");
-}
+
 
 
 
