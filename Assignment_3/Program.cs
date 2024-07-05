fd0374606b5f28645a67ac0f0f8a546ec8af6573
San123desh <suuchang53@gmail.com>
2024-07-05 10:47:51 +0545

    handle nullable type

--- Assignment_3/Program.cs
+++ Assignment_3/Program.cs
@@ -1,11 +1,21 @@
-﻿var inflationAnalysis = new InflationAnalysis("Book1.csv");
+﻿var inflationAnalysis = new InflationAnalysis("Inflation.csv");
 
-var inflationRatesForYear = inflationAnalysis.GetInflationRatesForYear(2020);
 
-Console.WriteLine("Inflation Rates for Year 2020:");
-foreach (var inflationRate in inflationRatesForYear)
+// var inflationRatesForYear = inflationAnalysis.GetInflationRatesForYear(2021);
+// Console.WriteLine("Inflation Rates for Year 2021:");
+// foreach (var inflationRate in inflationRatesForYear)
+// {
+//     Console.WriteLine($"{inflationRate.RegionalMember}:{inflationRate.InflationRate}");
+// }
+
+int? highestInflationYear = inflationAnalysis.GetYearWithHighestInflation();
+if (highestInflationYear.HasValue)
 {
-    Console.WriteLine($"{inflationRate.RegionalMember}:{inflationRate.InflationRate}");
+    Console.WriteLine($"\nYear when Nepal had the highest inflation: {highestInflationYear.Value}");
+}
+else
+{
+    Console.WriteLine("\nNo data available for Nepal.");
 }
 
 
